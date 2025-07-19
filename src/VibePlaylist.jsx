82c94b5efff693dd7a4f88afa7bd37c4a74fc3f9
vazfo <150393193+vazfo@users.noreply.github.com>
2025-07-19 19:12:36 -0400

    Create VibePlaylist.jsx

---
+++ src/VibePlaylist.jsx
@@ -0,0 +1,15 @@
+export default function VibePlaylist() {
+  return (
+    <div style={{
+      background: "black",
+      color: "lime",
+      fontSize: "2rem",
+      minHeight: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
+    }}>
+      <p>Hello from Vibe 25!</p>
+    </div>
+  );
+}

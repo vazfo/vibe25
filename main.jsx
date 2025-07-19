d339bc9534bbf8f446dc38460fe76a043df623e8
vazfo <150393193+vazfo@users.noreply.github.com>
2025-07-19 18:39:53 -0400

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import VibePlaylist from './VibePlaylist'
+import './SpotifyThemeCSS.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <VibePlaylist />
+  </React.StrictMode>
+)

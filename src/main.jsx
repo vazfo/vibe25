24f23adb1c3597a1f4035139051c8c32edf58c59
vazfo <150393193+vazfo@users.noreply.github.com>
2025-07-19 19:11:31 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import VibePlaylist from "./VibePlaylist";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <VibePlaylist />
+  </React.StrictMode>
+);

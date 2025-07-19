a545ab388e35ca46712fe2a3e61ec800a58aec02
vazfo <150393193+vazfo@users.noreply.github.com>
2025-07-19 18:56:10 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,2 +1,11 @@
-src/main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import VibePlaylist from "./VibePlaylist";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <VibePlaylist />
+  </React.StrictMode>
+);
+
 
